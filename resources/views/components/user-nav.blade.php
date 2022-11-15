5908bf96b29915fe1aaaee05f95000140bfb0dd2
nldinee <nbd.0x0539@gmail.com>
2022-11-15 20:34:44 +0100

    Fixing calendar evenets for demp and icons for user-navbar

--- resources/views/components/user-nav.blade.php
+++ resources/views/components/user-nav.blade.php
@@ -30,7 +30,7 @@
             <ul class="nav-main">
 				<li class="nav-main-item">
                     <a class="nav-main-link {{Route::is('resume') ? 'active' : ''}}" href="{{route('resume')}}">
-                        <i class="nav-main-link-icon si si-speedometer"></i>
+                        <i class="nav-main-link-icon far fa-user"></i>
                         <span class="nav-main-link-name">Resume</span>
                     </a>
                 </li>
@@ -42,14 +42,14 @@
                 </li>
                 <li class="nav-main-item">
                     <a class="nav-main-link {{Route::is(['courses.index','courses.detail']) ? 'active' : ''}}" href="{{route('courses.index')}}">
-                        <i class="nav-main-link-icon si si-speedometer"></i>
+                        <i class="nav-main-link-icon si si-book-open"></i>
                         <span class="nav-main-link-name">Courses</span>
                     </a>
                 </li>
 
                 <li class="nav-main-item">
                     <a class="nav-main-link {{Route::is(['test_prep_dashboard']) ? 'active' : ''}}" href="{{route('test_prep_dashboard')}}">
-                        <i class="nav-main-link-icon si si-speedometer"></i>
+                        <i class="nav-main-link-icon si si-puzzle"></i>
                         <span class="nav-main-link-name">Test Prep Dashboard</span>
                     </a>
                 </li>
