4fc507d3a2a3dab2d3437c2b8636b988936b5496
shahbazkhan06 <129360664+shahbazkhan06@users.noreply.github.com>
2023-04-24 12:27:54 +0530

    resume changes by shahbaz
    added model for cities and states tables

---
+++ app/Models/HighSchoolResume/Cities.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models\HighSchoolResume;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cities extends Model
+{
+    use HasFactory;
+    public $timestamps = false;
+    protected $table = 'cities';
+
+}
---
+++ app/Models/HighSchoolResume/States.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models\HighSchoolResume;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class States extends Model
+{
+    use HasFactory;
+    public $timestamps = false;
+    protected $table = 'states';
+
+}
