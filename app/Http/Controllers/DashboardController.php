68d3826adc6f3c8abd818aa421aba95e88679f50
Arbi Syarifudin <arbisyarifudin@gmail.com>
2024-03-19 14:21:16 +0700

    refactor(user): update dashboard college list display & fix admission deadline date

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -18,23 +18,149 @@ use App\Service\DashboardService;
 
 class DashboardController extends Controller
 {
-    public function __construct(DashboardService $dashboardService) {
+    private $dashboardService;
+
+    public function __construct(DashboardService $dashboardService)
+    {
         $this->dashboardService = $dashboardService;
     }
 
     public function dashboard()
-	{
+    {
+        // dd($this->dashboardService->college_list_deadline());
+        $college_list_deadlines = $this->dashboardService->college_list_deadline();
+
+        foreach ($college_list_deadlines as $key => $college_list_deadline) {
+            $college_list_deadlines[$key]['deadline_date'] = $this->__getDeadlineDate($college_list_deadline);
+        }
+
+        // dd($college_list_deadlines);
+
         return view('user.dashboard', [
             'getTestScores' => $this->dashboardService->get_test_scores(),
             'events' => $this->dashboardService->events(),
             'final_arr' => $this->dashboardService->all_events(),
             'milestones' => $this->dashboardService->milestones(),
-            'college_list_deadline' => $this->dashboardService->college_list_deadline(),
+            'college_list_deadline' => $college_list_deadlines,
             'google_calendar' => $this->dashboardService->google_calendar(),
         ]);
-	}
+    }
 
-    public function test_prep_dashboard() {
+    private function __getDeadlineDate($deadline)
+    {
+        $deadlineDate = null;
+
+        if (
+            isset($deadline['college_deadline']['admissions_deadline']) &&
+            !empty($deadline['college_deadline']['admissions_deadline'])
+        ) {
+            $deadlineDate = $deadline['college_deadline']['admissions_deadline']; // month-day-year
+            // convert to Y-m-d
+            $dateExplode = explode('-', $deadlineDate);
+            $deadlineDate = $dateExplode[2] . '-' . $dateExplode[0] . '-' . $dateExplode[1];
+            // echo '0--> '. $deadlineDate . '<br>';
+        } else {
+            $adminissionOptionSelected = $deadline['college_deadline']['admission_option']; // Early Action, Early Decision 1, Early Decision 2, Regular Decision, Rolling Admission
+
+            if (!empty($adminissionOptionSelected)) {
+                // admission deadline:
+                // Early Action: AP_DL_EACT_DAY, AP_DL_EACT_MON
+                // Early Decision 1: APL_DL_EDEC_1_DAY, APL_DL_EDEC_1_MON
+                // Early Decision 2: APL_DL_EDEC_2_DAY, APL_DL_EDEC_2_MON
+                // Regular Decision: APL_DL_FRSH_DAY, APL_DL_FRSH_MON
+                // Rolling Admission: No
+
+                $collegeInformation = $deadline['college_information'];
+
+                $deadlineDay = 0;
+                $deadlineMonth = 0;
+
+                if ($adminissionOptionSelected == 'Early Action') {
+                    $deadlineDay = $collegeInformation['early_action_day'] ?: $collegeInformation['AP_DL_EACT_DAY'];
+                    $deadlineMonth = $collegeInformation['early_action_month'] ?: $collegeInformation['AP_DL_EACT_MON'];
+                } elseif ($adminissionOptionSelected == 'Early Decision 1') {
+                    $deadlineDay =
+                        $collegeInformation['early_decision_i_day'] ?: $collegeInformation['APL_DL_EDEC_1_DAY'];
+                    $deadlineMonth =
+                        $collegeInformation['early_decision_i_month'] ?: $collegeInformation['APL_DL_EDEC_1_MON'];
+                } elseif ($adminissionOptionSelected == 'Early Decision 2') {
+                    $deadlineDay =
+                        $collegeInformation['early_decision_ii_day'] ?: $collegeInformation['APL_DL_EDEC_2_DAY'];
+                    $deadlineMonth =
+                        $collegeInformation['early_decision_ii_month'] ?: $collegeInformation['APL_DL_EDEC_2_MON'];
+                } elseif ($adminissionOptionSelected == 'Regular Decision') {
+                    $deadlineDay =
+                        $collegeInformation['regular_decision_day'] ?: $collegeInformation['APL_DL_FRSH_DAY'];
+                    $deadlineMonth =
+                        $collegeInformation['regular_decision_month'] ?: $collegeInformation['APL_DL_FRSH_MON'];
+                } elseif ($adminissionOptionSelected == 'Rolling Admission') {
+                    //
+                }
+
+                if ($deadlineDay && $deadlineMonth) {
+                    $year = date('Y');
+                    $date = strtotime($year . '-' . $deadlineMonth . '-' . $deadlineDay);
+                    if ($date < time()) {
+                        $deadlineDate = $deadlineMonth . '-' . $deadlineDay . '-' . ($year + 1);
+                    } else {
+                        $deadlineDate = $deadlineMonth . '-' . $deadlineDay . '-' . $year;
+                    }
+                } else {
+                    $deadlineDate = isset($collegeInformation['regular_admission_deadline'])
+                        ? $collegeInformation['regular_admission_deadline']
+                        : '';
+
+                    if (!empty($deadlineDate)) {
+                        $deadlineDate = date('Y-m-d', strtotime($deadlineDate));
+                    }
+                }
+            }
+        }
+
+
+        $deadlineDateDiff = 0; // + or -
+        $deadlineDateDiffLabel = ''; // Due in 123 days (dynamic)
+        if (!empty($deadlineDate)) {
+            // echo '1--> '. $deadlineDate . '<br>';
+            $deadlineDateDiff = strtotime($deadlineDate) - time();
+
+            if ($deadlineDateDiff > 0) {
+                $deadlineDateDiffLabel = 'Due in ' . floor($deadlineDateDiff / (60 * 60 * 24)) . ' days';
+            } else {
+
+                // add 1 years from deadlineDate
+                // $deadlineDate = date('Y-m-d', strtotime('+1 year', strtotime($deadlineDate)));
+                // echo '2 --> '. $deadlineDate . '<br>';
+                // $deadlineDateDiff = strtotime($deadlineDate) - time();
+                // $deadlineDateDiffLabel = 'Due in ' . floor($deadlineDateDiff / (60 * 60 * 24)) . ' days';
+
+                // set past due label
+                $dueCountDay = floor(abs($deadlineDateDiff) / (60 * 60 * 24));
+                $deadlineDateDiffLabel = 'Past due ' . $dueCountDay . ' days';
+            }
+        }
+
+        if (!empty($deadlineDate)) {
+            return [
+                'date' => $deadlineDate,
+                'dateInput' => date('m-d-Y', strtotime($deadlineDate)),
+                'dateLabel' => date('F d, Y', strtotime($deadlineDate)),
+                'diff' => $deadlineDateDiff,
+                'diffLabel' => $deadlineDateDiffLabel,
+            ];
+        }
+
+        return [
+            'date' => '',
+            'dateInput' => '',
+            'dateLabel' => '',
+            'diff' => '',
+            'diffLabel' => '',
+        ];
+    }
+
+    public function test_prep_dashboard()
+    {
         return view('student.test-prep-dashboard.dashboard', [
             'getTestScores' => $this->dashboardService->get_test_scores(),
             'events' => $this->dashboardService->events(),
@@ -46,7 +172,8 @@ class DashboardController extends Controller
         ]);
     }
 
-    public function admission_dashboard() {
+    public function admission_dashboard()
+    {
         return view('user.admin-dashboard.dashboard', [
             'college_list_deadline' => $this->dashboardService->college_list_deadline(),
             'worksheet_data' => $this->dashboardService->worksheet_data(),
--- resources/views/components/admission/dashboard-college-list.blade.php
+++ resources/views/components/admission/dashboard-college-list.blade.php
@@ -2,9 +2,9 @@
 
     @if (count($college_list_deadline) > 0)
         @foreach ($college_list_deadline as $key => $deadline)
-            <div class="col-12" style="height : 271px!important; "  id="{{$deadline['college_deadline']['id']}}">
-                <div style="min-height:100%!important;  "
-                    class="block block-bordered shadow py-3 px-2 gap-1 d-flex flex-column align-items-center  @if (!$deadline['college_information']['regular_admission_deadline']) @endif">
+            <div class="col-12" style="height : 290px !important; " id="{{ $deadline['college_deadline']['id'] }}">
+                <div style="min-height:100% !important;  "
+                    class="block block-bordered shadow py-3 px-2 gap-1 d-flex flex-column align-items-center  @if (!$deadline['college_information']['regular_admission_deadline'])  @endif">
                     @if ($deadline['college_information']['college_icon'])
                         <img class="college-image"
                             src="{{ asset('college_icon/' . $deadline['college_information']['college_icon']) }}"
@@ -14,18 +14,24 @@
                     @endif
                     <div class="fs-sm fw-semibold text-muted text-uppercase">Choice #{{ $key + 1 }}</div>
                     <a class="text-dark text-center">{{ $deadline['college_name'] }}</a>
-                    <div class="fs-sm fw-semibold text-muted text-uppercase">Admissions Deadline</div>
-                    @if ($deadline['college_information']['regular_admission_deadline'])
-                    <div class="deadline-div text-center">
-                        <span class="text-dark d-block">{{ $deadline['college_information']['regular_admission_deadline'] }}</span>
-                        <span class="text-dark d-block">{{ $deadline['college_deadline']['admissions_deadline_diff'] }}</span>
-                    </div>
-                    @else
-                    <div class="deadline-div text-center">
-                        <span class="text-danger d-block">Not Published</span>
-                    </div>
+                    @if (!empty($deadline['deadline_date']['date']))
+                        @if($deadline['deadline_date']['date'])
+                        <div class="fs-sm fw-semibold text-muted text-uppercase">Admissions Deadline</div>
+                        <div class="fs-xs text-muted text-italic">{{ $deadline['college_deadline']['admission_option'] }}</div>
+                        <div class="deadline-div text-center">
+                            <span class="text-dark d-block">{{ $deadline['deadline_date']['dateLabel'] }}</span>
+                            <span class="text-dark d-block fs-xs {{ $deadline['deadline_date']['diff'] < 0 ? 'text-danger' : '' }}">{{ $deadline['deadline_date']['diffLabel'] }}</span>
+                        </div>
+                        @else
+                        <div class="deadline-div text-center">
+                            <span class="text-danger d-block">Not Published</span>
+                        </div>
+                        @endif
                     @endif
-                    <button type="button" class="btn btn-sm btn-alt-secondary mt-2 manage-deadline" data-bs-toggle="modal" data-bs-target="#deadline-modal" data-dead-line="{{$deadline['college_information']['regular_admission_deadline']}}" data-deadline-id="{{$deadline['college_deadline']['id']}}">Edit deadline</button>
+                    <button type="button" class="btn btn-sm btn-alt-secondary mt-2 manage-deadline"
+                        data-bs-toggle="modal" data-bs-target="#deadline-modal"
+                        data-dead-line="{{ @$deadline['deadline_date']['dateInput'] }}"
+                        data-deadline-id="{{ $deadline['college_deadline']['id'] }}">{{ !empty($deadline['deadline_date']['diff']) ? 'Edit' : 'Add' }} deadline</button>
                 </div>
             </div>
         @endforeach
@@ -34,12 +40,14 @@
         $total_element = count($college_list_deadline) == 0 ? 1 : 1;
     @endphp
     @for ($i = 0; $i < $total_element; $i++)
-        <div class="col-12" style="height:271px!important" >
-            <div class="block block-bordered shadow py-3 px-2 gap-1 d-flex flex-column align-items-center box-height" style="height : 100%;" >
+        <div class="col-12" style="height:271px!important">
+            <div class="block block-bordered shadow py-3 px-2 gap-1 d-flex flex-column align-items-center box-height"
+                style="height : 100%;">
                 <img class="college-image" src="{{ asset('static-image/college-logo.png') }}" alt="">
                 <div class="fs-sm fw-semibold text-muted text-uppercase">Choice
                     #{{ count($college_list_deadline) + $i + 1 }}</div>
-                <a href="{{ route('admin-dashboard.initialCollegeList.step1') }}" class="btn btn-alt-success btn-sm">Add
+                <a href="{{ route('admin-dashboard.initialCollegeList.step1') }}"
+                    class="btn btn-alt-success btn-sm">Add
                     college</a>
             </div>
         </div>
@@ -55,13 +63,14 @@
             <div class="modal-body">
                 <div>
                     <label class="form-label" for="admissions_deadline-0">Admissions Deadline</label>
-                    <input type="text" class="deadline-date form-control update-form"
-                        data-index="0" value="mm/dd/yy" placeholder="mm/dd/yy" />
+                    <input type="text" class="deadline-date form-control update-form" data-index="0" value="mm/dd/yy"
+                        placeholder="mm/dd/yy" />
                 </div>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary update-deadline" onclick="updateDeadline(this.dataset['id'], this.dataset['date'])">Save changes</button>
+                <button type="button" class="btn btn-primary update-deadline"
+                    onclick="updateDeadline(this.dataset['id'], this.dataset['date'])">Save changes</button>
             </div>
         </div>
     </div>
